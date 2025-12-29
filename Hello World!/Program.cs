3490785cadc7951c8cf1e66c265fb25d29e138be
asilkanmcelik <aslkanceli07@gmail.com>
2025-12-30 00:06:32 +0300

    week 2 homework finalized

--- Hello World!/Program.cs
+++ Hello World!/Program.cs
@@ -1,4 +1,6 @@
-﻿#region 1.Gün/ 1.Ders
+﻿
+#region 1. Hafta
+#region 1.Gün/ 1.Ders
 //Console.WriteLine(":D");
 //Console.Writline yazı sunumlandırmada kullanılıyor. Artı olarak noktalı virgüle dikkat.CTRL+k+c
 #endregion
@@ -91,68 +93,263 @@ using System.ComponentModel.Design;
 //string ile kendinizi anlattığınız bir kompozisyon yazıyorsunuz. Buradali parametreler yaş maaş vb. en uzun yazan +5 puan(I dont know what that means.. Gıthub bağlamaya +3 puan githuba projesini bağlayana +3 puan daha)
 #endregion
 
-#region HW
-int degiskenSayisi = 0; degiskenSayisi++;
-string deneme = "deneme"; degiskenSayisi++;
-int int1 = 1; degiskenSayisi++;
-int int2 = 2; degiskenSayisi++;
-int inttop12 = (int1 + int2); degiskenSayisi++;
-int int3 = 3; degiskenSayisi++;
-int int4 = 4; degiskenSayisi++;
-int int5 = 5; degiskenSayisi++;
-int int6 = 6; degiskenSayisi++;
-int int7 = 7; degiskenSayisi++;
-int int8 = 8; degiskenSayisi++;
-int int9 = 9; degiskenSayisi++;
-int int10 = 10; degiskenSayisi++;
-double doubleSayi1 = 1.11; degiskenSayisi++;
-double doubleSayi2 = 2.22; degiskenSayisi++;
-double doubleSayi3 = 3.33; degiskenSayisi++;
-double doubleSayi4 = 4.44; degiskenSayisi++;
-double doubleSayi5 = 5.55; degiskenSayisi++;
-double doubleSayi6 = 6.66; degiskenSayisi++;
-double doubleSayi7 = 7.77; degiskenSayisi++;
-double doubleSayi8 = 8.88; degiskenSayisi++;
-double doubleSayi9 = 9.99; degiskenSayisi++;
-double doubleSayi10 = 10.10; degiskenSayisi++;
-double doubleSayi11 = 11.11; degiskenSayisi++;
-double doubleSayi12 = 12.12; degiskenSayisi++;
+#region 1. Ödev
+//int degiskensayisi = 0; degiskensayisi++;
+//string deneme = "deneme"; degiskensayisi++;
+//int int1 = 1; degiskensayisi++;
+//int int2 = 2; degiskensayisi++;
+//int inttop12 = (int1 + int2); degiskensayisi++;
+//int int3 = 3; degiskensayisi++;
+//int int4 = 4; degiskensayisi++;
+//int int5 = 5; degiskensayisi++;
+//int int6 = 6; degiskensayisi++;
+//int int7 = 7; degiskensayisi++;
+//int int8 = 8; degiskensayisi++;
+//int int9 = 9; degiskensayisi++;
+//int int10 = 10; degiskensayisi++;
+//double doublesayi1 = 1.11; degiskensayisi++;
+//double doublesayi2 = 2.22; degiskensayisi++;
+//double doublesayi3 = 3.33; degiskensayisi++;
+//double doublesayi4 = 4.44; degiskensayisi++;
+//double doublesayi5 = 5.55; degiskensayisi++;
+//double doublesayi6 = 6.66; degiskensayisi++;
+//double doublesayi7 = 7.77; degiskensayisi++;
+//double doublesayi8 = 8.88; degiskensayisi++;
+//double doublesayi9 = 9.99; degiskensayisi++;
+//double doublesayi10 = 10.10; degiskensayisi++;
+//double doublesayi11 = 11.11; degiskensayisi++;
+//double doublesayi12 = 12.12; degiskensayisi++;
 
-string adSoyad = "Asilkan Çelik"; degiskenSayisi++;
-string Okul = "ODTU"; degiskenSayisi++;
-string bolum = "Fizik"; degiskenSayisi++;
-int sinif = 3;
-string calısma = ""; degiskenSayisi++;
-string maas = "0"; degiskenSayisi++;
-bool calismadurumu = false; degiskenSayisi++;
-if (calismadurumu == false)
-    calısma = "calısmıyorum";   
-else
-    calısma = "calısıyorum";
-    
-int dogumGunu = 27; degiskenSayisi++;
-int dogumAyi = 12; degiskenSayisi++;
-int dogumYili = 2000; degiskenSayisi++;
-string ilgiAlani1 = "felsefe"; degiskenSayisi++;
-string ilgiAlani2 = "müzik"; degiskenSayisi++;
-string ilgiAlani3 = "finans"; degiskenSayisi++;
-Console.WriteLine("Ödev 1");
-Console.WriteLine("Stringler string yazılarak kulanılır. Diğer type türleri de küçük harflerle başlar " +
-"tanımlanan yapılarsa küçük başlar ama devamındaki her sözcük tercihen büyük harfle kullanılmalıdır. Örnek olarak " +deneme +" string'inin nasıl kullanıldığına bakılabilir. " +
-"Derste öğrendiğimiz bir diğer yapı ise toplamadır. Kodda görebileceğiniz üzere kullanım şekli " + int1+"+" +int2 +"=" + (inttop12) + " şeklindedir." +
-" 1 eksik integer değeri kullanarak yapabileceğimiz metodsa şu şekildedir: " + int3 +"+" +int4 +"="+ (int3+int4) +" .Sıra diğer işlem türlerine geldi. Çıkarma " + int9+"-"+int10+"="+(int9-int10)+
-"şeklinde, çarpma " +int5+"x"+int6+"=" +(int5*int6)+
-" şeklinde, son olarak bölme " +int7 +"/" +int8+"="+ (int7/int8)+" şeklinde yapılır. " +
-"Görebileceğiniz üzere bölme işlemi sadece bir integera sıkıştırılmak zorunda olduğu için bir basamaklık değeri verilmiştir ve doğruluktan oldukça uzaktır. Daha detaylı işlem kapasitesi için double değişkenler tercih edilebilir." +
-" Örneğin toplamada "+doubleSayi1+"+"+doubleSayi2+"=" +(doubleSayi1+doubleSayi2)+ " ve çıkarmada " + doubleSayi3 + "-" + doubleSayi4 + "=" + (doubleSayi3 - doubleSayi4) + " metod aynen kullanılır. Ama görebileceğiniz üzere decimal yerine double kullanmamızdan ötürü bazı sayılar tahmini ve yanlış sonuçlar doğurabilir. " +
-"Daha net sonuçlar için decimallar tercih edilmeli. Çarpım "+ doubleSayi5 +"x" +doubleSayi6 +"="+ (doubleSayi5*doubleSayi6) +" şeklinde yapılır. " +
-"Bölme ise " +doubleSayi7+"/"+doubleSayi8 +(doubleSayi7/doubleSayi8) +" şeklinde yapılır." +" Daha karmaşık işlemler ise işlem önceliği sırasına göre tasarlanı. " +
-"Örnek olarak (("+ doubleSayi9+"+" +doubleSayi10+")"+"x"+doubleSayi11+")/"+ doubleSayi12+ "=" +(((doubleSayi9+doubleSayi10)*doubleSayi11)/doubleSayi12)+" denklemi verilebilir. Double olması yine işlemlerde düşük ölçekte hataya sebep vermektedir. Ödev kısmı bittiğine göre şimdi kendimden bahsedebilirim." +
-" Ben " + adSoyad + "." + Okul+ "'de "+ sinif +". sınıf " + bolum + "öğrencisiyim. " +dogumGunu+"." + dogumAyi + "." + dogumYili + "'de doğdum. Şu sıralar hobilerim " + ilgiAlani1+", " + ilgiAlani2 + "," + ilgiAlani3 + " olarak sıralanabilir. Öğrenci olduğum için şu sıralar " + calısma +". O nedenle şu an maaşım " +
-maas +" tl." + " Aktif bir şekilde kariyerimi yazılıma yönlendirmek için oldukça heyecanlı ve kararlıyım. Son olarak değisken sayısı: " + degiskenSayisi);
+//string adsoyad = "asilkan çelik"; degiskensayisi++;
+//string okul = "odtu"; degiskensayisi++;
+//string bolum = "fizik"; degiskensayisi++;
+//int sinif = 3;
+//string calısma = ""; degiskensayisi++;
+//string maas = "0"; degiskensayisi++;
+//bool calismadurumu = false; degiskensayisi++;
+//if (calismadurumu == false)
+//    calısma = "calısmıyorum";   
+//else
+//    calısma = "calısıyorum";
+
+//int dogumgunu = 27; degiskensayisi++;
+//int dogumayi = 12; degiskensayisi++;
+//int dogumyili = 2000; degiskensayisi++;
+//string ilgialani1 = "felsefe"; degiskensayisi++;
+//string ilgialani2 = "müzik"; degiskensayisi++;
+//string ilgialani3 = "finans"; degiskensayisi++;
+//console.writeline("ödev 1");
+//console.writeline("stringler string yazılarak kulanılır. diğer type türleri de küçük harflerle başlar " +
+//"tanımlanan yapılarsa küçük başlar ama devamındaki her sözcük tercihen büyük harfle kullanılmalıdır. örnek olarak " +deneme +" string'inin nasıl kullanıldığına bakılabilir. " +
+//"derste öğrendiğimiz bir diğer yapı ise toplamadır. kodda görebileceğiniz üzere kullanım şekli " + int1+"+" +int2 +"=" + (inttop12) + " şeklindedir." +
+//" 1 eksik integer değeri kullanarak yapabileceğimiz metodsa şu şekildedir: " + int3 +"+" +int4 +"="+ (int3+int4) +" .sıra diğer işlem türlerine geldi. çıkarma " + int9+"-"+int10+"="+(int9-int10)+
+//"şeklinde, çarpma " +int5+"x"+int6+"=" +(int5*int6)+
+//" şeklinde, son olarak bölme " +int7 +"/" +int8+"="+ (int7/int8)+" şeklinde yapılır. " +
+//"görebileceğiniz üzere bölme işlemi sadece bir integera sıkıştırılmak zorunda olduğu için bir basamaklık değeri verilmiştir ve doğruluktan oldukça uzaktır. daha detaylı işlem kapasitesi için double değişkenler tercih edilebilir." +
+//" örneğin toplamada "+doublesayi1+"+"+doublesayi2+"=" +(doublesayi1+doublesayi2)+ " ve çıkarmada " + doublesayi3 + "-" + doublesayi4 + "=" + (doublesayi3 - doublesayi4) + " metod aynen kullanılır. ama görebileceğiniz üzere decimal yerine double kullanmamızdan ötürü bazı sayılar tahmini ve yanlış sonuçlar doğurabilir. " +
+//"daha net sonuçlar için decimallar tercih edilmeli. çarpım "+ doublesayi5 +"x" +doublesayi6 +"="+ (doublesayi5*doublesayi6) +" şeklinde yapılır. " +
+//"bölme ise " +doublesayi7+"/"+doublesayi8 +(doublesayi7/doublesayi8) +" şeklinde yapılır." +" daha karmaşık işlemler ise işlem önceliği sırasına göre tasarlanı. " +
+//"örnek olarak (("+ doublesayi9+"+" +doublesayi10+")"+"x"+doublesayi11+")/"+ doublesayi12+ "=" +(((doublesayi9+doublesayi10)*doublesayi11)/doublesayi12)+" denklemi verilebilir. double olması yine işlemlerde düşük ölçekte hataya sebep vermektedir. ödev kısmı bittiğine göre şimdi kendimden bahsedebilirim." +
+//" ben " + adsoyad + "." + okul+ "'de "+ sinif +". sınıf " + bolum + "öğrencisiyim. " +dogumgunu+"." + dogumayi + "." + dogumyili + "'de doğdum. şu sıralar hobilerim " + ilgialani1+", " + ilgialani2 + "," + ilgialani3 + " olarak sıralanabilir. öğrenci olduğum için şu sıralar " + calısma +". o nedenle şu an maaşım " +
+//maas +" tl." + " aktif bir şekilde kariyerimi yazılıma yönlendirmek için oldukça heyecanlı ve kararlıyım. son olarak değisken sayısı: " + degiskensayisi);
 
 
 
 
 
 #endregion
+#endregion
+
+#region 2. Hafta
+#region döngüler
+//4 adet döngü var for, for each, while, Do while.
+//string[] ogrenciler= { "Bedirhan", "Samet", "Nisa", "Zarife", "Asilkan", "Mert", "Aylin", "Ferhat" };
+
+//for (int i = 0; i < ogrenciler.Length; i++)  //Breakpoint ile her bir adım test etmeyi öğrendik.
+//{
+//    Console.WriteLine(ogrenciler[i]);
+//}
+
+//int[] sayilar = { 10, 15, 20, 35, 50, 70, 90 };
+//int toplam = 0;
+//int toplam2 = 0;
+//for (int i = 0;i < sayilar.Length; i++)
+//{
+//    toplam += sayilar[i];
+//    if (toplam <= 70)
+//    {
+//        Console.WriteLine($"{i} index kontrol edildi. 70'ten küçük. Toplam Sayı {toplam}");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"{i} index kontrol edildi. 70'ten büyük. Toplam sayı {toplam}");
+//    }
+
+
+//}
+#endregion
+
+#region Kullanıcı girdileri ile işlem
+//Console.Write("Adınızı giriniz: ");
+//string ad=Console.ReadLine();
+//Console.Write("Soyadınızı giriniz: ");
+//string soyad = Console.ReadLine();
+//Console.Write("Yaşınız: ");
+//int yas = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"Adınız: {ad} Soyadınız: {soyad} Yaşınız: {yas}");
+
+#endregion
+
+#region Kısaltmalar ve eklemeler
+//Console.Write("Kaç öğrenci gireceksiniz: ");
+//int deger = int.Parse(Console.ReadLine());
+//string[] ogrenciler = new string[deger];
+
+//for (int i = 0; i < deger; i++)
+//{
+//    Console.Write($"{i + 1}. öğrenci adını giriniz: ");
+//    ogrenciler[i] = Console.ReadLine();
+//}
+//Console.WriteLine($"öğrenciler: {string.Join(",", ogrenciler)}");
+//int sayac = 0;
+//foreach (var ogrenci in ogrenciler)
+//{
+//    //TODO: içinde C harfi geçen öğrenci sayısı
+//    if (ogrenci.ToLower().Contains("c")) 
+//    {
+//        sayac++;
+//        Console.WriteLine(ogrenci);
+//    }
+
+//}
+//Console.WriteLine($"içerisinde Charfi geçen öğrenciler: {sayac}");
+#endregion
+
+#region Ödev 1 eleman talebine göre dizi oluşturma (Aralık kontrollü) 
+//Console.Write("Dizideki eleman sayısı (N): ");
+//int N= int.Parse(Console.ReadLine());
+//while (N <= 0)
+//{
+//    if (N <= 0)
+//    {
+//        Console.WriteLine("1 veya daha fazla eleman sayısı ile tekrar deneyiniz.");
+//        Console.Write("Dizideki eleman sayısı (N): ");
+//        N = int.Parse(Console.ReadLine());
+//    }
+//}
+
+
+
+//int[] dizi = new int[N];
+//for (int i = 0; i < N; i++)
+//    {
+//        Console.Write($"{i + 1}. eleman: ");
+//        dizi[i] = int.Parse(Console.ReadLine());
+//    }
+//    for (int i = 0; i < N; i++)
+//    {
+//        Console.WriteLine($"{i+1}. index: {dizi[i]}");
+//    }
+#endregion
+#region Ödev 2 TEKRAR ET!!!!!!
+//Console.Write("Dizideki eleman sayısı (N): ");
+//int N = int.Parse(Console.ReadLine());
+
+//while (N <= 0)
+//{
+//    Console.WriteLine("N 1 veya daha büyük olmalı.");
+//    Console.Write("Dizideki eleman sayısı (N): ");
+//    N = int.Parse(Console.ReadLine());
+//}
+
+//int[] dizi = new int[N];
+
+//for (int i = 0; i < N; i++)
+//{
+//    Console.Write($"{i + 1}. eleman: ");
+//    dizi[i] = int.Parse(Console.ReadLine());
+//}
+//int numberOfEvens = 0;
+//int numberOfOdds = 0;
+
+//foreach (int number in dizi)
+//{
+//    if (number % 2 == 0) numberOfEvens++;
+//    else numberOfOdds++;
+//}
+//int[] evens = new int[numberOfEvens];
+//int[] odds = new int[numberOfOdds];
+//int eIndex = 0;
+//int oIndex = 0;
+
+//foreach (int number in dizi)
+//{
+//    if (number % 2 == 0)
+//    {
+//        evens[eIndex] = number; // evens dizisinin eIndex'inci yerine number yaz
+//        eIndex++;
+//    }
+//    else
+//    {
+//        odds[oIndex] = number;  // odds dizisinin oIndex'inci yerine number yaz
+//        oIndex++;
+//    }
+//}
+//Console.Write("Ciftler: ");
+//for (int i = 0; i < evens.Length; i++)
+//    Console.Write(evens[i] + " ");
+//Console.WriteLine();
+
+//Console.Write("Tekler: ");
+//for (int i = 0; i < odds.Length; i++)
+//    Console.Write(odds[i] + " ");
+//Console.WriteLine();
+
+//Console.WriteLine($"Cift sayisi = {numberOfEvens}, Tek sayisi = {numberOfOdds}");
+#endregion
+#region Ödev 3
+
+//bool tekrar = true;
+//while (tekrar == true)
+//{
+//    Console.Write("Dizideki eleman sayısı (N): ");
+//    int N = int.Parse(Console.ReadLine());
+//    while (N <= 0)
+//    {
+//        Console.WriteLine("N 1 veya daha büyük olmalı.");
+//        Console.Write("Dizideki eleman sayısı (N): ");
+//        N = int.Parse(Console.ReadLine());
+//    }
+//    int[] dizi = new int[N];
+
+//    for (int i = 0; i < N; i++)
+//    {
+//        Console.Write($"{i + 1}. eleman: ");
+//        dizi[i] = int.Parse(Console.ReadLine());
+//    }
+//    int min = dizi[0];
+//    int max = dizi[0];
+//    foreach (int d in dizi)
+//    {
+//        if (d < min) min = d;
+//        else if (d > max) max = d;
+
+//    }
+//    Console.WriteLine($"minimum değer: {min},maksimum değer: {max}");
+//    Console.Write($"Başka bir diziyi denemek ister misiniz?(y/n)");
+//    string s= Console.ReadLine();
+//    if (s == "y") tekrar = true;
+//    else if (s == "n");
+//    else
+//    {
+//        break;
+//    }
+//}
+
+
+
+#endregion
+#endregion
+
+
+
